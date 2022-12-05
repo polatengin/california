d2c3dfbd0608632bafbe290139a4bd39a9b64574
Engin Polat <polatengin@hotmail.com>
2022-12-05 01:56:51 +0000

    adding host and join sub commands

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,2 +1,24 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.CommandLine;
+using Microsoft.AspNetCore.SignalR.Client;
+
+var hostOption = new Option<bool>(name: "--host", description: "To host a bingo game");
+var joinOption = new Option<string>(name: "--join", description: "To host a bingo game");
+
+var rootCommand = new RootCommand();
+var sub1Command = new Command("host", "First-level subcommand");
+rootCommand.Add(sub1Command);
+
+sub1Command.SetHandler(() =>
+{
+  Console.WriteLine("host");
+});
+
+var sub2Command = new Command("join", "Second level subcommand");
+rootCommand.Add(sub2Command);
+
+sub2Command.SetHandler(() =>
+{
+  Console.WriteLine("join");
+});
+
+await rootCommand.InvokeAsync(args);
