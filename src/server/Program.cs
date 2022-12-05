5dadcdf5e56b8f089b295277ca3a650fa5ed66a5
Engin Polat <polatengin@hotmail.com>
2022-12-05 01:56:21 +0000

    adding bingohub mapping

--- src/server/Program.cs
+++ src/server/Program.cs
@@ -1,3 +1,5 @@
+using Bingo.Server.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSignalR();
@@ -5,5 +7,7 @@ builder.Services.AddSignalR();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapHub<BingoHub>("/hub");
 
 app.Run();
