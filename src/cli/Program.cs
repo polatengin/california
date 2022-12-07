46a29271037d68d60526493a06545e8c0f57fb89
Engin Polat <polatengin@hotmail.com>
2022-12-07 05:20:04 +0000

    adding host and join commands

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -2,49 +2,136 @@ using System.CommandLine;
 using System.Runtime.InteropServices;
 using Microsoft.AspNetCore.SignalR.Client;
 
-var hostOption = new Option<bool>(name: "--host", description: "To host a bingo game");
-var joinOption = new Option<string>(name: "--join", description: "To host a bingo game");
-
-var rootCommand = new RootCommand();
-var sub1Command = new Command("host", "First-level subcommand");
-rootCommand.Add(sub1Command);
-
-sub1Command.SetHandler(() =>
-{
-  Console.WriteLine("host");
-});
-
-var sub2Command = new Command("join", "Second level subcommand");
-rootCommand.Add(sub2Command);
-
-sub2Command.SetHandler(() =>
-{
-  Console.WriteLine("join");
-});
+var _gameName = string.Empty;
+var _players = new HashSet<Tuple<string, string>>();
 
 var connection = new HubConnectionBuilder()
     .WithUrl("http://localhost:5000/hub")
+    .WithAutomaticReconnect()
     .Build();
+
+async Task ConnectAsync()
+{
+  try
+  {
+    await connection.StartAsync();
+  }
+  catch (Exception ex)
+  {
+    Console.WriteLine(ex.Message);
+
+    Environment.Exit(1);
+  }
+}
+
+async Task RenderMenuAsync()
+{
+  Console.Clear();
+
+  Console.WriteLine("List of people connected to the game:");
+
+  foreach (var client in _players)
+  {
+    Console.WriteLine(client.Item2);
+  }
+
+  Console.WriteLine("Press any key to start the game");
+
+  Console.ReadLine();
+
+  await connection.InvokeAsync("StartGame", _gameName);
+}
 
 connection.Closed += async (error) =>
 {
-  Console.WriteLine("Connection closed");
-  await Task.Delay(new Random().Next(10, 15) * 1000);
-  Console.WriteLine("retrying...");
-  await connection.StartAsync();
-  Console.WriteLine("Connected to server");
+  Console.WriteLine(await Task.FromResult("Connection closed"));
 };
 
-connection.On<string, string>("JoinGroup", (user, message) =>
+connection.On<HashSet<Tuple<string, string>>>("UpdatePlayerList", async (players) =>
 {
-  Console.WriteLine($"{user}: {message}");
+  _players = players;
+
+  await RenderMenuAsync();
 });
 
-await connection.StartAsync();
+var rootCommand = new RootCommand();
 
-Console.WriteLine("Connected to server");
+var playerNameOption = new Option<string>("--player-name", "The name of the player");
+playerNameOption.AddAlias("-p");
 
-await connection.InvokeAsync("JoinGroup", "test");
+var gameNameOption = new Option<string>("--game-name", "The name of the game");
+gameNameOption.AddAlias("-g");
+
+var hostCommand = new Command("host", "Host a new bingo game with your friends");
+hostCommand.AddOption(gameNameOption);
+hostCommand.AddOption(playerNameOption);
+rootCommand.Add(hostCommand);
+
+hostCommand.SetHandler(async (gameName, playerName) =>
+{
+  if (string.IsNullOrWhiteSpace(playerName))
+  {
+    playerName = $"Anonymous {Random.Shared.Next(0, 1000)}";
+  }
+
+  if (string.IsNullOrWhiteSpace(gameName))
+  {
+    gameName = Guid.NewGuid().ToString();
+  }
+
+  _gameName = gameName;
+
+  await ConnectAsync();
+
+  Console.WriteLine("Connected to server");
+
+  var result = await connection.InvokeAsync<bool>("CreateGroup", gameName, playerName);
+
+  if (result)
+  {
+    await RenderMenuAsync();
+  }
+  else
+  {
+    Console.WriteLine($"Game {gameName} already exists");
+  }
+
+}, gameNameOption, playerNameOption);
+
+var joinCommand = new Command("join", "Join a bingo game");
+joinCommand.AddOption(gameNameOption);
+joinCommand.AddOption(playerNameOption);
+rootCommand.Add(joinCommand);
+
+joinCommand.SetHandler(async (gameName, playerName) =>
+{
+  if (string.IsNullOrWhiteSpace(playerName))
+  {
+    playerName = $"Anonymous {Random.Shared.Next(0, 1000)}";
+  }
+
+  if (string.IsNullOrWhiteSpace(gameName))
+  {
+    Console.WriteLine("Please provide a game name");
+    return;
+  }
+
+  _gameName = gameName;
+
+  await ConnectAsync();
+
+  var result = await connection.InvokeAsync<bool>("JoinGroup", gameName, playerName);
+
+  if (result)
+  {
+    await RenderMenuAsync();
+  }
+  else
+  {
+    Console.WriteLine($"Game {gameName} does not exist");
+  }
+
+}, gameNameOption, playerNameOption);
 
 await rootCommand.InvokeAsync(args);
 var tcs = new TaskCompletionSource();
